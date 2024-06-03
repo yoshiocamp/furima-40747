5236b96c0317a767d36116795bb1eef5161c03b9
yoshiocamp <yoshio14393974@gmail.com>
2024-06-03 22:40:04 +0900

    マイグレーションファイルの設定

--- db/migrate/20240530081455_devise_create_users.rb
+++ db/migrate/20240530081455_devise_create_users.rb
@@ -4,8 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, unique: true 
       t.string :encrypted_password, null: false, default: ""
+      t.string :first_name,         null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name,          null: false
+      t.string :last_name_kana,     null: false
+      t.string :nickname,           null: false
+      t.date   :birth,              null: false
 
       ## Recoverable
       t.string   :reset_password_token
