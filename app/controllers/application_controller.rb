410341b4b71d294a7b0d70f0cf600eb208ec0626
yoshiocamp <yoshio14393974@gmail.com>
2024-06-04 12:22:28 +0900

    ログインの有無で表示を変える記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
 
+def new
+end
+
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to root_path
+  else
+    render 'new'
+  end
+end
 
   private
 
