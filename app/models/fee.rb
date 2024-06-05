7c45c1d025794fb2b2ec2f39e08e7ffb3558fa6f
yoshiocamp <yoshio14393974@gmail.com>
2024-06-05 23:25:50 +0900

    ActiveHashを導入

---
+++ app/models/fee.rb
@@ -0,0 +1,11 @@
+class fee < ActiveHash::Base
+  self.data = [
+   { id: 1, name: '---' },
+   { id: 2, name: '着払い（購入者負担）' },
+   { id: 3, name: '未使用に近い' },
+
+   ]
+    include ActiveHash::Associations
+    has_many :items
+ 
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :delivery
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :status
+  
   has_one_attached :image
 end
