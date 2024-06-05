dc5266b16955c03b20dff22adb8b7fb617c549b7
yoshiocamp <yoshio14393974@gmail.com>
2024-06-06 00:11:09 +0900

    バリテーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,21 @@ class Item < ApplicationRecord
   belongs_to :fee
   belongs_to :prefecture
   belongs_to :status
-  
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ , message: "は半角（数字）で入力してください"},numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は¥300~¥9,999,999の間で設定してください' }
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :fee_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
+  validates :status_id, numericality: { other_than: 1, message: "can't be blank" } 
+
+  validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
