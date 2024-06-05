402a80718cc92e0a040def3256b60ccec21101eb
yoshiocamp <yoshio14393974@gmail.com>
2024-06-05 22:25:27 +0900

    ActiveStorageを導入

--- Gemfile
+++ Gemfile
@@ -82,3 +82,5 @@ group :production do
 end
 
 gem "devise"
+gem 'mini_magick'
+gem 'image_processing', '~> 1.2'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,7 @@
 class ItemsController < ApplicationController
+  private
 
+  def message_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
