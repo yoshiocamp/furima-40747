2c525e3ad6b4a929871bca1f4ee0117e17bb52c9
yoshiocamp <yoshio14393974@gmail.com>
2024-06-07 18:02:07 +0900

    商品一覧の表示と並び替え

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
