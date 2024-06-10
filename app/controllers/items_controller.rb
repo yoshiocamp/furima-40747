b1622bd63df81b9b3786260c4ad989bc286c5cea
yoshiocamp <yoshio14393974@gmail.com>
2024-06-10 16:44:29 +0900

    パスの変更と before_actionの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new]
+  before_action :authenticate_user!, only:[:new, :edit]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def index
@@ -22,6 +22,17 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    
+   
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def show
