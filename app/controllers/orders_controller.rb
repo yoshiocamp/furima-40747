d7fe4b83fd862866f968c05e3a15b7dafb42203e
yoshiocamp <yoshio14393974@gmail.com>
2024-06-16 21:45:31 +0900

    set itemの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,17 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only:[:index ]
+  before_action :set_item,only:[:index, :create ]
 
   def index
        gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
        @order = Order.new
-       @item = Item.find(params[:item_id])
        @order_address = OrderAddress.new
        if current_user.id == @item.user.id || @item.order.present?
           redirect_to root_path
        end
   end
 
-  def create 
-    @item = Item.find(params[:item_id]) 
+  def create  
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
@@ -25,6 +24,10 @@ class OrdersController < ApplicationController
   end
   
   private
+
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
   
     def order_params
       # params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number,:token, :item_id).merge(user_id: current_user.id, item_id: params[:item_id])
